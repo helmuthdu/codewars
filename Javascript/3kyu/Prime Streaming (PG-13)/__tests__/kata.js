2812dfe33950f96096f0b03417f2313c5739f9bd
h.saatkamp <helmuth.saatkamp@leverton.ai>
2019-01-17 16:10:41 +0100

    added new kata: "Prime Streaming"

---
+++ Javascript/3kyu/Prime Streaming (PG-13)/__tests__/kata.js
@@ -0,0 +1,14 @@
+import { Primes } from '../kata';
+
+function verify(from_n, ...vals) {
+  const stream = Primes.stream();
+  for (let i = 0; i < from_n; ++i) stream.next();
+  for (let v of vals) expect(stream.next().value).toEqual(v);
+}
+
+describe('Small numbers', () => {
+  it('0_10', () => verify(0, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29));
+  it('10_10', () => verify(10, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71));
+  it('100_10', () => verify(100, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601));
+  it('1000_10', () => verify(1000, 7927, 7933, 7937, 7949, 7951, 7963, 7993, 8009, 8011, 8017));
+});
---
+++ Javascript/3kyu/Prime Streaming (PG-13)/kata.js
@@ -0,0 +1,45 @@
+const isPrime = input => {
+  let start = 2;
+  const limit = Math.sqrt(input);
+  while (start <= limit) {
+    if (input % start++ < 1) return false;
+  }
+  return input > 1;
+};
+
+export class Primes {
+  static stream(start = 0, end = Infinity, step = 1) {
+    let nextIndex = start;
+    let iterationCount = 0;
+
+    return {
+      next() {
+        while (!isPrime(iterationCount++)) {}
+
+        if (nextIndex <= end) {
+          nextIndex += step;
+          return { value: iterationCount - 1, done: false };
+        }
+
+        return { value: iterationCount - 1, done: true };
+      }
+    };
+  }
+}
+
+// alternative
+
+// export class Primes {
+//   static * stream(n = 100) {
+//     const primes = n < 2 ? [] : [2];
+//
+//     for (let i = 3; i <= n; i += 2) {
+//       if (primes.every(x => i % x)) primes.push(i);
+//     }
+//
+//     // replace this with your solution
+//     for(let p of primes) {
+//       yield p;
+//     }
+//   }
+// }
